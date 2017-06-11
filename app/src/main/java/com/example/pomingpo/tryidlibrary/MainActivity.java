cb934dac6321f88621e5db164791b504b4c94282
poming\pomingpo <roy989898@gmail.com>
2017-06-11 13:14:19 +0800

    add tv id

--- app/src/main/java/com/example/pomingpo/tryidlibrary/MainActivity.java
+++ app/src/main/java/com/example/pomingpo/tryidlibrary/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.pomingpo.tryidlibrary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView tvShow;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvShow = (TextView) findViewById(R.id.tv_show);
     }
 }
